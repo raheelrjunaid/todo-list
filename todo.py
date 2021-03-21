abfdf8dac07b67d6397664affe2d89a98de66cea
Raheel Junaid <raheelj2004@gmail.com>
2021-03-21 09:26:29 -0600

    Add vetter error handling when writing to file

--- todo.py
+++ todo.py
@@ -24,42 +24,44 @@ with open(json_file) as outfile:
     try:
         read = json.load(outfile)
     except json.decoder.JSONDecodeError:
-        read = {
-            "todos": []
-        }
+        read = {"todos": []}
+        print("JSONError: File is empty, creating base template.")
 
 if len(clargs) >= 1:
     with open(json_file, 'w') as outfile:
-        write = lambda data: json.dump(data, outfile, indent=2)
-        if "-n" in clargs:
-            new_todo = input("Add a new todo: ")
-            new_todo = {
-                "title": new_todo,
-                "time": str(datetime.now()),
-                "flag": True if "-f" in clargs else False,
-                "status": "incomplete"
-            }
+        try:
+            write = lambda data: json.dump(data, outfile, indent=2)
+            if "-n" in clargs:
+                new_todo = input("Add a new todo: ")
+                new_todo = {
+                    "title": new_todo,
+                    "date_created": str(datetime.now()),
+                    # TODO Add flag creation method
+                    "flag": True if "-f" in clargs else False,
+                    "status": "incomplete"
+                }
 
-            read['todos'].append(new_todo)
-            listTodos(read)
+                read['todos'].append(new_todo)
+                listTodos(read)
 
-        if len(read['todos']) > 0:
-            if clargs[0] == '-d':
-                if len(clargs) >= 2 and clargs[1].isdigit():
-                    try:
-                        deleteTodo(int(clargs[1]))
-                        listTodos(read)
-                    except (IndexError, ValueError) as e:
-                        print("Invalid option")
-                else:
+            if len(read['todos']) > 0:
+                # TODO Delete & complete multiple todos
+                if clargs[0] == '-d':
                     listTodos(read)
-                    number = input("Which todo? ")
-                    try:
-                        deleteTodo(int(number))
-                    except (IndexError, ValueError) as e:
-                        print("Invalid option")
-        else:
-            print("Please add todo to proceed.")
-        write(read)
+                    number = input("Which todo to delete? ")
+                    deleteTodo(int(number))
+                elif clargs[0] == '-c':
+                    listTodos(read)
+                    number = input("Which todo to complete? ")
+            else:
+                print("Please add todo to proceed.")
+        except (ValueError, IndexError) as e:
+            print(e, "Invalid Option")
+        except KeyboardInterrupt:
+            print("\nExited Program, todos preserved")
+        except Exception:
+            print(Exception, "\nFailed")
+        finally:
+            write(read)
 else:
     listTodos(read)
